d6878031fe8aa1c8da3325d78a5888d7a770542a
RAJ <verreauxblack@outlook.com>
2021-07-02 12:08:56 +0530

    pwa process successfully completed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as themes from './theme/schema.json';
 import { setToLS } from  './utils/storage';
 
+
 const Index = () => {
     setToLS('all-themes', themes.default);
     setToLS('theme', themes.data.theme1);
